c99056a44aff427b9cd45f940ac2d3316a6ecdac
insekticid <insekticid@exploit.cz>
2017-09-29 20:22:56 +0200

    fixed config via copy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-if [ ! -d config ]; then
-	cp -r ../config.backup/ config
-	chown -R www-data:www-data config
+if [ ! -e config/global.php ]; then
+	cp -r ../config.backup/* config/
+	chown -R www-data:www-data config/
 	echo "Piwik Config via copy"
 	#tar cf - --one-file-system -C /var/www/html . | tar xf -
 	#chown -R www-data .
