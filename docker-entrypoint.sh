c38db18e0c68ef61411c2c272daddc300c2005d7
Exploit.cz <github@exploit.cz>
2018-09-10 13:45:21 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,31 +4,17 @@ set -e
 mkdir -p config config/environment
 chown -R www-data:www-data config/
 
-if [ -e ../config.backup/global.ini.php ]; then
-	cp -r ../config.backup/global.ini.php config/
-	cp -r ../config.backup/global.php config/
-	cp -r ../config.backup/environment config/
-fi
+if [ $CRON_ENABLED ]; then
+	bash -c '
+	bash -s <<EOF
+	trap "break;exit" SIGHUP SIGINT SIGTERM
+	while /bin/true; do
+	  su -s "/bin/bash" -c "php /var/www/html/piwik/console core:archive" www-data
+	  sleep 600
+	done
+	EOF' & exec "$@"
 
-if [ ! -e config/config.ini.php ]; then
-	mkdir -p config
-	chown -R www-data:www-data config/
-	cp -r ../config.backup/* config/
-	echo "Piwik Config via copy"
-	#tar cf - --one-file-system -C /var/www/html . | tar xf -
-	#chown -R www-data .
-	
+	wait
+else
 	exec "$@"
 fi
-
-
-bash -c '
-bash -s <<EOF
-trap "break;exit" SIGHUP SIGINT SIGTERM
-while /bin/true; do
-  su -s "/bin/bash" -c "php /var/www/html/piwik/console core:archive" www-data
-  sleep 3600
-done
-EOF' & exec "$@"
-
-wait
